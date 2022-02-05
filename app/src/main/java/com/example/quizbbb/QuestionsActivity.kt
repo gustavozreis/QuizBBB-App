071258790bf3dade35d59c84e716160fd3b92040
gustavozreis <gzreis@gmail.com>
2022-02-05 00:39:33 -0300

    Inicio do projeto -inicio da UI das perguntas.

---
+++ app/src/main/java/com/example/quizbbb/QuestionsActivity.kt
@@ -0,0 +1,15 @@
+package com.example.quizbbb
+
+import android.app.Activity
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class QuestionsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_questions)
+    }
+
+}
